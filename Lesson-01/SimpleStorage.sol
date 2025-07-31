0a4f0777f2e35391a21e857467bf7f048cc11ba9
vahidabolhassani2 <vahid.abolhassani2@gmail.com>
2025-07-31 12:27:28 +0330

    c5

--- Lesson-01/SimpleStorage.sol
+++ Lesson-01/SimpleStorage.sol
@@ -2,5 +2,10 @@
 pragma solidity 0.8.7;
 
 contract SimpleStorage {
+    uint256 public favoriteNumber; // state variable
+    address public favoriteAddress;
+    string public favoriteString;
+    bool public favoriteBool;
+    
     
 }
