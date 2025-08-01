5636648e9c5362aaf6ec2094490c552285702e6d
vahidabolhassani2 <vahid.abolhassani2@gmail.com>
2025-08-01 09:14:34 +0330

    c-202508010914

--- Lesson-01/SimpleStorage.sol
+++ Lesson-01/SimpleStorage.sol
@@ -2,13 +2,35 @@
 pragma solidity 0.8.7;
 
 contract SimpleStorage {
-    uint256 public favoriteNumber; // state variable
-    address public favoriteAddress;
-    string public favoriteString;
-    bool public favoriteBool;
+    uint256 favoriteNumber; // state variable
+    // address public favoriteAddress;
+    // string public favoriteString;
+    // bool public favoriteBool;
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    People[] public people;
+
+    // uint256[] public favoriteNumbersList;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
         favoriteNumber += 1;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        // people.push(People(_favoriteNumber,_name));
+        People memory newPerson = People({
+            favoriteNumber: _favoriteNumber,
+            name: _name
+        });
+        people.push(newPerson);
+        // people.push(People(_favoriteNumber,_name));
+        // favoriteNumbersList.push(_favoriteNumber);
     }
 }
