e17872524e341db58ddb8c039d99caf97f93d681
vahidabolhassani2 <vahid.abolhassani2@gmail.com>
2025-08-02 14:03:08 +0330

    c-202508021403

---
+++ FundMe/Lesson04/FundMe.sol
@@ -0,0 +1,12 @@
+//Get funds from users
+//Withdraw funds
+//Set a minimum funding value in USD
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract FUndMe {
+    function fund() public {}
+
+    // function withdraw() {}
+}
