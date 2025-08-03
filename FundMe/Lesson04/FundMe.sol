c303841a989721d6af20f49216ee888a8612a404
vahidabolhassani2 <vahid.abolhassani2@gmail.com>
2025-08-03 11:26:45 +0330

    c-202508031126

--- FundMe/Lesson04/FundMe.sol
+++ FundMe/Lesson04/FundMe.sol
@@ -8,12 +8,25 @@ import {AggregatorV3Interface} from "@chainlink/contracts@1.4.0/src/v0.8/shared/
 
 contract FUndMe {
     // Address(ETH/USD): 0x694AA1769357215DE4FAC081bf1f309aDC325306
+    uint256 public minimumUsd = 50 * 1e18;
 
     function fund() public payable {
-        require(msg.value > 1e18, "not enought fund");
+        require(
+            getConversionRate(msg.value /*in wei*/) >= minimumUsd,
+            "Didn't send enough"
+        );
     }
 
-    function getPrice() public {}
+    function getPrice() public view returns (uint256) {
+        //ETH interms of USD
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+
+        uint8 weiPower = 18;
+        return uint256(answer) * 10**(weiPower - priceFeed.decimals());
+    }
 
     function getVersion() public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
@@ -22,6 +35,23 @@ contract FUndMe {
         return priceFeed.version();
     }
 
-    function getConversionRate() public {}
+    /*
+     * Converts input ETH to USD
+     */
+    function getConversionRate(uint256 ethAmount)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; //ethPrice*ethAmount has 36 0's
+        return ethAmountInUsd;
+        /*
+         *example:
+         * ethPrice = 3000 USD = 3000_000000000000000000 (in wei)
+         * ethAmount = 1 ETH = 1_000000000000000000 (in wei)
+         * ethAmountInUsd = 3000_000000000000000000
+         */
+    }
     // function withdraw() {}
 }
